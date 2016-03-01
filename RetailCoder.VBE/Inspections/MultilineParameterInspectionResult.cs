c57de52a1471bc4ce275b14ce4b5eed13e8a2770
Mathieu Guindon <retailcoder@gmail.com>
2016-02-29 22:27:06 -0500

    fixed multiline inspection

--- RetailCoder.VBE/Inspections/MultilineParameterInspectionResult.cs
+++ RetailCoder.VBE/Inspections/MultilineParameterInspectionResult.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Antlr4.Runtime;
 using Rubberduck.Parsing;
+using Rubberduck.Parsing.Symbols;
 using Rubberduck.UI;
 using Rubberduck.VBEditor;
 
@@ -8,11 +9,13 @@ namespace Rubberduck.Inspections
 {
     public class MultilineParameterInspectionResult : InspectionResultBase
     {
+        private readonly Declaration _target;
         private readonly IEnumerable<CodeInspectionQuickFix> _quickFixes;
 
-        public MultilineParameterInspectionResult(IInspection inspection, ParserRuleContext context, QualifiedMemberName qualifiedName)
-            : base(inspection, qualifiedName.QualifiedModuleName, context)
+        public MultilineParameterInspectionResult(IInspection inspection, Declaration target)
+            : base(inspection, target)
         {
+            _target = target;
             _quickFixes = new[]
             {
                 new MakeSingleLineParameterQuickFix(Context, QualifiedSelection), 
